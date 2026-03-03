776e8b2dfbab3b1a6d3da9f006695624166e25db
YuanJin <yuanjinmoke@gmail.com>
2026-03-03 12:08:42 +0800

    fix: 修复了c模块的fn1和fn2函数的console.log语句

--- c.js
+++ c.js
@@ -1,7 +1,7 @@
 function fn1(...args) {
-  console.log('c1', args);
+  console.log('cmodule : c1', args);
 }
 
 function fn2(...args) {
-  console.log('c2', args);
+  console.log('cmodule : c2', args);
 }
