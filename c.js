c536f2c08c525f8fc1e9721ee6a6c14fca5f85ec
YuanJin <yuanjinmoke@gmail.com>
2026-03-03 12:09:16 +0800

    fix: 修复了c模块的无导出函数的问题

--- c.js
+++ c.js
@@ -1,7 +1,7 @@
-function fn1(...args) {
+export function fn1(...args) {
   console.log('cmodule : c1', args);
 }
 
-function fn2(...args) {
+export function fn2(...args) {
   console.log('cmodule : c2', args);
 }
