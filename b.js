71ec949562781c13f878aeb66b6c5dd639eea5b7
Yuan Jin <79248401+yuanjin-stack@users.noreply.github.com>
2026-03-05 14:34:05 +0800

    Refactor functions to arrow function syntax

--- b.js
+++ b.js
@@ -1,7 +1,7 @@
-export function fn1(...args) {
+export const fn1 = (...args) => {
   console.log('module b: fn1', ...args);
-}
+};
 
-export function fn2(...args) {
+export const fn2 = (...args) => {
   console.log('module b: fn2', ...args);
-}
+};
